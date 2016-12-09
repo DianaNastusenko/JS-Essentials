317b22ad0d602a736173a60659c308a94c0ca685
DianaNastusenko <d-nastusenko@ukr.net>
2016-12-09 17:59:02 +0200

    change title and display #

--- 3/script.js
+++ 3/script.js
@@ -1,12 +1,8 @@
 function main() {
-    for (var i = 1; i < 10; i++) {
-        for (var j = 0; j < 10; j++) {
-            if (i == j) {
-                break;
-            }
-            console.log("#");
-        }
-        console.log("\n");
+    var str = '';
+    for (var i = 0; i < 10; i++) {
+        str += '#';
+        console.log(str);
     }
 }
 
